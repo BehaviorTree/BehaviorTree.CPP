eb8c97f31d9731f88b741379852d5eb8cbaf7921
Michele Colledanchise <miccol@kth.se>
2015-05-02 11:38:04 +0200

    Update test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -9,15 +9,14 @@ int main(int argc, char **argv)
         int TickPeriod_milliseconds = 1000;
 
         ActionTestNode* test1 = new ActionTestNode("A1");
-        ActionTestNode*test2 = new ActionTestNode("A2");
-        ActionTestNode*test3 = new ActionTestNode("A3");
-        ActionTestNode*test4 = new ActionTestNode("A4");
+        ActionTestNode* test2 = new ActionTestNode("A2");
+        ActionTestNode* test3 = new ActionTestNode("A3");
+        ActionTestNode* test4 = new ActionTestNode("A4");
 
         SequenceStarNode* sequence1 = new SequenceStarNode("seq1");
         SelectorStarNode* selector1 = new SelectorStarNode("sel1");
         SelectorStarNode* selector2 = new SelectorStarNode("sel12");
 
-        DecoratorRetryNode* dec = new DecoratorRetryNode("retry",2);
         SequenceStarNode* root = new SequenceStarNode("root");
 
         test1->SetBehavior(Success);
@@ -29,11 +28,12 @@ int main(int argc, char **argv)
 
         selector1->AddChild(test1);
         selector1->AddChild(test2);
+        
+        selector2->AddChild(test3);
+        selector2->AddChild(test4);
 
-        dec->AddChild(test1);
-
-        root->AddChild(test3);
         root->AddChild(selector1);
+        root->AddChild(selector2);
 
         Execute(root, TickPeriod_milliseconds);//from BehaviorTree.cpp
 }
