c9b1ab0d2ff3aeeb2ab78b7766bca9f8421934d6
Davide Faconti <davide.faconti@gmail.com>
2018-11-21 09:39:54 +0100

    removed M_PI

--- examples/t04_blackboard.cpp
+++ examples/t04_blackboard.cpp
@@ -41,7 +41,7 @@ const std::string xml_text = R"(
 // Use this function to create a SimpleActionNode that can access the blackboard
 NodeStatus CalculateGoalPose(TreeNode& self)
 {
-    const Pose2D mygoal = {1, 2, M_PI};
+    const Pose2D mygoal = {1.1, 2.3, 1.54};
 
     // RECOMMENDED: check if the blackboard is nullptr first
     if (self.blackboard())
