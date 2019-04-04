68b7da74248d068a93970ab38611791282ed2797
Davide Facont <davide.faconti@gmail.com>
2019-04-04 13:52:54 +0200

    fix issue #84 (Directories)

--- examples/t01_build_your_first_tree.cpp
+++ examples/t01_build_your_first_tree.cpp
@@ -70,7 +70,7 @@ int main()
 #else
     // Load dynamically a plugin and register the TreeNodes it contains
     // it automated the registering step.
-    factory.registerFromPlugin("./libdummy_nodes.so");
+    factory.registerFromPlugin("./libdummy_nodes_dyn.so");
 #endif
 
     // Trees are created at deployment-time (i.e. at run-time, but only once at the beginning).
