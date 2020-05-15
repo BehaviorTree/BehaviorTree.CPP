505c136acee6d867d1336df0326eff1b470d0ca3
Francisco Martin Rico <fmrico@gmail.com>
2020-05-15 21:57:12 +0200

    Adding author
    Signed-off-by: Francisco Martin Rico <fmrico@gmail.com>

--- include/behaviortree_cpp_v3/decorators/force_running_node.h
+++ include/behaviortree_cpp_v3/decorators/force_running_node.h
@@ -1,4 +1,5 @@
 /*  Copyright (C) 2018-2020 Davide Faconti, Eurecat -  All Rights Reserved
+*   Copyright (C) 2020 Francisco Martin, Intelligent Robotics Lab (URJC) <fmrico@gmail.com>
 *
 *   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
 *   to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
