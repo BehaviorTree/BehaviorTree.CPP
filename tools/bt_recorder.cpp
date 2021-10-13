9d9fc1342c0dcfe023120be1686a7ccdb9ad1f75
Akash <Ace314159@users.noreply.github.com>
2021-10-13 02:39:13 -0500

    Add signal handler for Windows (#307)

--- tools/bt_recorder.cpp
+++ tools/bt_recorder.cpp
@@ -16,12 +16,17 @@ static void s_signal_handler(int)
 
 static void CatchSignals(void)
 {
+#ifdef _WIN32
+    signal(SIGINT, s_signal_handler);
+    signal(SIGTERM, s_signal_handler);
+#else
     struct sigaction action;
     action.sa_handler = s_signal_handler;
     action.sa_flags = 0;
     sigemptyset(&action.sa_mask);
     sigaction(SIGINT, &action, nullptr);
     sigaction(SIGTERM, &action, nullptr);
+#endif
 }
 
 int main(int argc, char* argv[])
