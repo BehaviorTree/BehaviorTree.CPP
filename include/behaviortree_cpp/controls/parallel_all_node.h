ff1bfcd1248ef17447269d8f476e2815629ed2c9
Davide Faconti <davide.faconti@gmail.com>
2023-06-22 09:54:41 +0200

    Fix Issue 593 (#594): support skipping in Parallel node

--- include/behaviortree_cpp/controls/parallel_all_node.h
+++ include/behaviortree_cpp/controls/parallel_all_node.h
@@ -1,5 +1,4 @@
-/* Copyright (C) 2015-2018 Michele Colledanchise -  All Rights Reserved
- * Copyright (C) 2018-2022 Davide Faconti, Eurecat -  All Rights Reserved
+/* Copyright (C) 2023 Davide Faconti -  All Rights Reserved
 *
 *   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
 *   to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
@@ -19,65 +18,45 @@
 namespace BT
 {
 /**
- * @brief The ParallelNode execute all its children
+ * @brief The ParallelAllNode execute all its children
  * __concurrently__, but not in separate threads!
  *
- * Even if this may look similar to ReactiveSequence,
- * this Control Node is the __only__ one that can have
- * multiple children RUNNING at the same time.
- *
- * The Node is completed either when the THRESHOLD_SUCCESS
- * or THRESHOLD_FAILURE number is reached (both configured using ports).
- *
- * If any of the thresholds is reached, and other children are still running,
- * they will be halted.
+ * It differs in the way ParallelNode works because the latter may stop
+ * and halt other children if a certain number of SUCCESS/FAILURES is reached,
+ * whilst this one will always complete the execution of ALL its children.
  *
  * Note that threshold indexes work as in Python:
  * https://www.i2tutorials.com/what-are-negative-indexes-and-why-are-they-used/
  *
  * Therefore -1 is equivalent to the number of children.
  */
-class ParallelNode : public ControlNode
+class ParallelAllNode : public ControlNode
 {
 public:
-  ParallelNode(const std::string& name);
 
-  ParallelNode(const std::string& name, const NodeConfig& config);
+  ParallelAllNode(const std::string& name, const NodeConfig& config);
 
   static PortsList providedPorts()
   {
-    return {InputPort<int>(THRESHOLD_SUCCESS, -1,
-                           "number of children which need to succeed to trigger a "
-                           "SUCCESS"),
-            InputPort<int>(THRESHOLD_FAILURE, 1,
-                           "number of children which need to fail to trigger a FAILURE")};
+    return {InputPort<int>("max_failures", 1,
+                           "If the number of children returning FAILURE exceeds this value, "
+                           "ParallelAll returns FAILURE")};
   }
 
-  ~ParallelNode() override = default;
+  ~ParallelAllNode() override = default;
 
   virtual void halt() override;
 
-  size_t successThreshold() const;
   size_t failureThreshold() const;
-  void setSuccessThreshold(int threshold);
   void setFailureThreshold(int threshold);
 
 private:
-  int success_threshold_;
-  int failure_threshold_;
-  
-  std::set<size_t> completed_list_;
+  size_t failure_threshold_;
 
-  size_t success_count_ = 0;
+  std::set<size_t> completed_list_;
   size_t failure_count_ = 0;
 
-  bool read_parameter_from_ports_;
-  static constexpr const char* THRESHOLD_SUCCESS = "success_count";
-  static constexpr const char* THRESHOLD_FAILURE = "failure_count";
-
   virtual BT::NodeStatus tick() override;
-
-  void clear();
 };
 
 }   // namespace BT
