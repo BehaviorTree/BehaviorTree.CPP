ab5f7ad23390812f50a99720ea4a8485af2f5870
Davide Faconti <davide.faconti@gmail.com>
2023-10-16 12:14:16 +0200

    fix

--- examples/ex05_subtree_model.cpp
+++ examples/ex05_subtree_model.cpp
@@ -1,7 +1,5 @@
 #include "behaviortree_cpp/bt_factory.h"
-#include "behaviortree_cpp/decorators/loop_node.h"
 #include "behaviortree_cpp/loggers/bt_cout_logger.h"
-#include <list>
 
 using namespace BT;
 
@@ -14,15 +12,15 @@ using namespace BT;
  * We are telling to the factory that the callee should remap
  * two mandatory inputs, called:
  *
- * - in_value (that has the default value 42)
- * - in_name (no default)
+ * - sub_in_value (that has the default value 42)
+ * - sub_in_name (no default)
  *
  * Similarly, there are two output values:
  *
- * - out_value (default remapping to port {output})
- * - out_state (no default)
+ * - sub_out_result (default remapping to port {output})
+ * - sub_out_state (no default)
  *
- * The callee MUST specify, at least, those remapping that have
+ * The callee MUST specify those remapping that have
  * no default value.
  */
 
@@ -32,17 +30,17 @@ static const char* xml_subtree = R"(
 
   <TreeNodesModel>
     <SubTree ID="MySub">
-      <input_port name="in_value" default="42"/>
-      <input_port name="in_name"/>
-      <output_port name="out_result" default="{output}"/>
-      <output_port name="out_state"/>
+      <input_port name="sub_in_value" default="42"/>
+      <input_port name="sub_in_name"/>
+      <output_port name="sub_out_result" default="{out_result}"/>
+      <output_port name="sub_out_state"/>
     </SubTree>
   </TreeNodesModel>
 
   <BehaviorTree ID="MySub">
     <Sequence>
-      <ScriptCondition code="in_name=='john' && in_value==42" />
-      <Script code="out_result:=69; out_state:='ACTIVE'" />
+      <ScriptCondition code="sub_in_value==42 && sub_in_name=='john'" />
+      <Script code="sub_out_result:=69; sub_out_state:='ACTIVE'" />
     </Sequence>
   </BehaviorTree>
 </root>
@@ -58,9 +56,10 @@ static const char* xml_maintree = R"(
 
   <BehaviorTree ID="MainTree">
     <Sequence>
-      <Script code="name_arg:= 'john' "/>
-      <SubTree ID="MySub" in_name="{name_arg}"  out_state="{state}"/>
-      <ScriptCondition code=" output==69 && state=='ACTIVE' " />
+      <Script code="in_name:= 'john' "/>
+      <SubTree ID="MySub" sub_in_name="{in_name}"
+                          sub_out_state="{out_state}"/>
+      <ScriptCondition code=" out_result==69 && out_state=='ACTIVE' " />
     </Sequence>
   </BehaviorTree>
 
@@ -83,10 +82,10 @@ int main()
 
   // The full remapping was:
   //
-  // - in_name <-> {name_arg}   // specified by callee
-  // - in_value <-> 42          // default
-  // - out_result <-> {output}  // default
-  // - out_state <-> {state}    // specified by callee
+  // - sub_in_name    <-> {in_name}      // specified by callee 'MainTree'
+  // - sub_in_value   <-> 42             // default remapping, see model
+  // - sub_out_result <-> {out_result}   // default remapping, see model
+  // - sub_out_state  <-> {out_state}    // specified by callee 'MainTree'
 
   return 0;
 }
