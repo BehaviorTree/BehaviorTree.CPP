d7457d22796a123f86372d555dec92de0ba60e62
Davide Faconti <davide.faconti@gmail.com>
2023-10-16 17:37:23 +0200

    fix warning in older compilers

--- include/behaviortree_cpp/utils/safe_any.hpp
+++ include/behaviortree_cpp/utils/safe_any.hpp
@@ -347,9 +347,9 @@ nonstd::expected<T, std::string> Any::stringToNumber() const
 {
   static_assert(std::is_arithmetic_v<T> && !std::is_same_v<T, bool>, "Expecting a numeric type");
 
-  auto str = linb::any_cast<SafeAny::SimpleString>(_any);
-  T out;
+  const auto str = linb::any_cast<SafeAny::SimpleString>(_any);
 #if __has_include(<GL/gl.h>)
+  T out;
   auto [ptr, err] = std::from_chars(str.data(), str.data() + str.size(), out);
   if(err == std::errc())
   {
