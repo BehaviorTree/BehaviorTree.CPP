BehaviorTree/BehaviorTree.CPP

commit d4d8ae13fabe82c88de7c1885ce226272015325a

Additional XML verification for ReactiveSequence nodes (#885)
* Additional XML verification for Control nodes

* Parse for async nodes based on node name

* Add a unit test

* Improve the check by counting num async children

* Minor update (const)

---------

Co-authored-by: AndyZe <andyzelenak@apptronik.com>

src/xml_parsing.cpp
@@ -541,6 +541,29 @@ void VerifyXML(const std::string& xml_text,
           ThrowError(line_number,
                      std::string("The node <") + name + "> must have 1 or more children");
         }
+        if(name == "ReactiveSequence")
+        {
+          size_t async_count = 0;
+          for(auto child = node->FirstChildElement(); child != nullptr;
+              child = child->NextSiblingElement())
+          {
+            const std::string child_name = node->FirstChildElement()->Name();
+            const auto child_search = registered_nodes.find(child_name);
+            const auto child_type = child_search->second;
+            if(child_type == NodeType::CONTROL &&
+               ((child_name == "ThreadedAction") ||
+                (child_name == "StatefulActionNode") ||
+                (child_name == "CoroActionNode") || (child_name == "AsyncSequence")))
+            {
+              ++async_count;
+              if(async_count > 1)
+              {
+                ThrowError(line_number, std::string("A ReactiveSequence cannot have more "
+                                                    "than one async child."));
+              }
+            }
+          }
+        }
       }
     }
     //recursion

tests/gtest_reactive.cpp
@@ -156,3 +156,31 @@ TEST(Reactive, TestLogging)
   ASSERT_EQ(observer.getStatistics("testA").success_count, num_ticks);
   ASSERT_EQ(observer.getStatistics("success").success_count, num_ticks);
 }
+
+TEST(Reactive, TwoAsyncNodesInReactiveSequence)
+{
+  static const char* reactive_xml_text = R"(
+<root BTCPP_format="4" >
+  <BehaviorTree ID="MainTree">
+    <ReactiveSequence>
+      <AsyncSequence name="first">
+        <TestA/>
+        <TestB/>
+        <TestC/>
+      </AsyncSequence>
+      <AsyncSequence name="second">
+        <TestD/>
+        <TestE/>
+        <TestF/>
+      </AsyncSequence>
+    </ReactiveSequence>
+  </BehaviorTree>
+</root>
+)";
+
+  BT::BehaviorTreeFactory factory;
+  std::array<int, 6> counters;
+  RegisterTestTick(factory, "Test", counters);
+
+  EXPECT_ANY_THROW(auto tree = factory.createTreeFromText(reactive_xml_text));
+}

commit b2fc82c8b59351baac7dbd82e4ab5588c3cb2517

fix issue #918 (introduced in #885)

src/xml_parsing.cpp
@@ -548,8 +548,13 @@ void VerifyXML(const std::string& xml_text,
           for(auto child = node->FirstChildElement(); child != nullptr;
               child = child->NextSiblingElement())
           {
-            const std::string child_name = node->FirstChildElement()->Name();
+            const std::string child_name = child->Name();
             const auto child_search = registered_nodes.find(child_name);
+            if(child_search == registered_nodes.end())
+            {
+              ThrowError(child->GetLineNum(),
+                         std::string("Unknown node type: ") + child_name);
+            }
             const auto child_type = child_search->second;
             if(child_type == NodeType::CONTROL &&
                ((child_name == "ThreadedAction") ||