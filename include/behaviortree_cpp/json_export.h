1e473fe5f3e9a4019620e1e06e16e8c3f59ac7f0
Davide Faconti <davide.faconti@gmail.com>
2025-04-17 18:56:02 +0200

    fixing comments

--- include/behaviortree_cpp/json_export.h
+++ include/behaviortree_cpp/json_export.h
@@ -80,22 +80,31 @@ public:
   template <typename T>
   Expected<T> fromJson(const nlohmann::json& source) const;
 
-  /// Register new JSON converters with addConverter<Foo>().
-  /// You should have used first the macro BT_JSON_CONVERTER
+  /**
+   * @brief Register new JSON converters with addConverter<Foo>().
+   * You should used first the macro BT_JSON_CONVERTER.
+   * The convertions from/to vector<T> are automatically registered.
+   */
   template <typename T>
   void addConverter();
 
   /**
    * @brief addConverter register a to_json function that converts a json to a type T.
+   * The convertion to std:vector<T> is automatically registered.
    *
    * @param to_json the function with signature void(const T&, nlohmann::json&)
    * @param add_type if true, add a field called [__type] with the name ofthe type.
-   * */
+   */
   template <typename T>
   void addConverter(std::function<void(const T&, nlohmann::json&)> to_json,
                     bool add_type = true);
 
-  /// Register custom from_json converter directly.
+  /**
+   * @brief addConverter register a from_json function that converts a json to a type T.
+   * The convertions from std::vector<T> is automatically registered.
+   *
+   * @param from_json the function with signature void(const nlohmann::json&, T&)
+   */
   template <typename T>
   void addConverter(std::function<void(const nlohmann::json&, T&)> from_json);
 
