6731c518f804e58147eb9698a03ae7a01f90ef93
Yiyi Wang <91304853+ahuo1@users.noreply.github.com>
2025-09-10 18:20:00 +0800

    fix: check path attribute before using (#1005)
    Co-authored-by: ahuo <ahuo2865189826@gmail.com>

--- src/xml_parsing.cpp
+++ src/xml_parsing.cpp
@@ -261,10 +261,16 @@ void XMLParser::PImpl::loadDocImpl(XMLDocument* doc, bool add_includes)
       break;
     }
 
+    const char* path_attr = incl_node->Attribute("path");
+    if(!path_attr)
+    {
+      throw RuntimeError("Invalid <include> tag: missing 'path' attribute");
+    }
+
 #if __bt_cplusplus >= 202002L
-    auto file_path(std::filesystem::path(incl_node->Attribute("path")));
+    auto file_path{ std::filesystem::path(path_attr) };
 #else
-    auto file_path(std::filesystem::u8path(incl_node->Attribute("path")));
+    auto file_path{ std::filesystem::u8path(path_attr) };
 #endif
 
     const char* ros_pkg_relative_path = incl_node->Attribute("ros_pkg");
