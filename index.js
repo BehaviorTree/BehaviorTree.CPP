c9b3d6194db0f84b3a16a62bbeeaafeec0d86a89
facontidavide <davide.faconti@gmail.com>
2026-02-04 10:51:49 +0000

    Deploying to gh-pages from @ BehaviorTree/BehaviorTree.CPP@e15354126d45efc146f0696f883476c459222594 🚀

--- index.js
+++ index.js
@@ -2,28 +2,9 @@ var index =
 [
     [ "Quick Start", "index.html#autotoc_md0", null ],
     [ "Core Concepts", "index.html#autotoc_md1", null ],
-    [ "Built-in Control Nodes", "index.html#autotoc_md2", [
-      [ "Sequences", "index.html#autotoc_md3", null ],
-      [ "Fallbacks", "index.html#autotoc_md4", null ],
-      [ "Parallels", "index.html#autotoc_md5", null ],
-      [ "Conditional", "index.html#autotoc_md6", null ]
-    ] ],
-    [ "Built-in Decorators", "index.html#autotoc_md7", [
-      [ "Repetition", "index.html#autotoc_md8", null ],
-      [ "Timing", "index.html#autotoc_md9", null ],
-      [ "Result Modification", "index.html#autotoc_md10", null ],
-      [ "Execution Control", "index.html#autotoc_md11", null ],
-      [ "Subtrees", "index.html#autotoc_md12", null ],
-      [ "Preconditions", "index.html#autotoc_md13", null ]
-    ] ],
-    [ "Built-in Actions", "index.html#autotoc_md14", [
-      [ "Status", "index.html#autotoc_md15", null ],
-      [ "Blackboard", "index.html#autotoc_md16", null ],
-      [ "Utility", "index.html#autotoc_md17", null ],
-      [ "Scripting", "index.html#autotoc_md18", null ],
-      [ "Entry Updated", "index.html#autotoc_md19", null ],
-      [ "Queue", "index.html#autotoc_md20", null ]
-    ] ],
-    [ "Logging & Tools", "index.html#autotoc_md21", null ],
-    [ "Resources", "index.html#autotoc_md22", null ]
+    [ "Built-in Control Nodes", "index.html#autotoc_md2", null ],
+    [ "Built-in Decorators", "index.html#autotoc_md3", null ],
+    [ "Built-in Actions", "index.html#autotoc_md4", null ],
+    [ "Logging & Tools", "index.html#autotoc_md5", null ],
+    [ "Resources", "index.html#autotoc_md6", null ]
 ];
